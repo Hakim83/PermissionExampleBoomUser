f48eec3d4638cc5f2ece30c8d535483b13ab34a4
Mohammed Abdalhakam <mohammed.hakam@gmail.com>
2019-02-19 14:06:12 +0200

    Initial commit

---
+++ app/src/main/java/course/example/permissionexampleboomuser/MainActivity.java
@@ -0,0 +1,25 @@
+package course.example.permissionexampleboomuser;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String BOOM_ACTION = "com.example.permissionexampleboom.BOOM_ACTION";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BOOM_ACTION));
+            }
+        });
+    }
+}
